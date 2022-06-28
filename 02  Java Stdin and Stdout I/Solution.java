cb0a08a30fa5f08a2b95ec415c547f559b96470c
Saurabh <saurabhchavhan07@gmail.com>
2022-06-28 16:17:14 +0530

    02 Second Problem

---
+++ 02  Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,34 @@
+// Input Format
+
+// There are 3 lines of input, and each line contains a single integer.
+
+// Sample Input
+
+// 42
+// 100
+// 125
+// Sample Output
+
+// 42
+// 100
+// 125
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
